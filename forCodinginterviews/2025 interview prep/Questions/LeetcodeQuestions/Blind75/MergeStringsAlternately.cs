07d99e5883756f60a874b9e04bbb4d8c049b6ee4
anantakhanal16 <anantaramankhanal@gmail.com>
2026-05-01 23:36:20 +0545

    solved merge string alternativly

---
+++ forCodinginterviews/2025 interview prep/Questions/LeetcodeQuestions/Blind75/MergeStringsAlternately.cs
@@ -0,0 +1,74 @@
+﻿//You are given two strings word1 and word2.
+//Merge the strings by adding letters in alternating order,
+//starting with word1. If a string is longer than the other,
+//append the additional letters onto the end of the merged string.
+
+//Return the merged string.
+//Example 1:
+
+//Input: word1 = "abc", word2 = "pqr"
+//Output: "apbqcr"
+//Explanation: The merged string will be merged as so:
+//word1: a b   c
+//word2:    p q   r
+//merged: a p b q c r
+//Example 2:
+
+//Input: word1 = "ab", word2 = "pqrs"
+//Output: "apbqrs"
+//Explanation: Notice that as word2 is longer, "rs" is appended to the end.
+//word1: a b
+//word2: p q   r s
+//merged: a p b q   r   s
+//Example 3:
+
+//Input: word1 = "abcd", word2 = "pq"
+//Output: "apbqcd"
+//Explanation: Notice that as word1 is longer, "cd" is appended to the end.
+//word1: a b   c d
+//word2:    p q 
+//merged: a p b q c   d
+
+
+//Constraints:
+
+//1 <= word1.length, word2.length <= 100
+//word1 and word2 consist of lowercase English letters.
+
+
+using System.Text;
+
+namespace forCodinginterviews._2025_interview_prep.Questions.LeetcodeQuestions.Blind75
+{
+    public static class Merge_Strings_Alternately
+    {
+        public static string MergeAlternately(string firstString , string secondString)
+        {
+            StringBuilder merged = new StringBuilder();
+
+            int i = 0;
+
+            while (i < firstString.Length || i < secondString.Length)
+            {
+                if (i < firstString.Length)
+                {
+                    merged.Append(firstString[i]);
+                }
+
+                if (i < secondString.Length)
+                {
+                    merged.Append(secondString[i]);
+                }
+
+                i++;
+
+            }
+
+            Console.WriteLine("first string: " + firstString);
+            Console.WriteLine("second string: " + secondString);
+            Console.WriteLine("Merged string: " + merged);
+
+            return merged.ToString();
+        }
+    }
+}
--- forCodinginterviews/Program.cs
+++ forCodinginterviews/Program.cs
@@ -1,14 +1,13 @@
-﻿using forCodinginterviews._2025_interview_prep.Questions.ArrayBasedEasyQuestions;
-using forCodinginterviews._2025_interview_prep.Questions.Others;
-
+﻿using forCodinginterviews._2025_interview_prep.Questions.LeetcodeQuestions.Blind75;
 
 class Program
 {
     public static void Main(string[] args)
     {
-        var number = 6;
-        var result = PrimeNumber.IsPrime(number);
-        Console.WriteLine($"result : {result}");
+        string firstCase = Merge_Strings_Alternately.MergeAlternately("abcd", "pq");
+        string secoandCase = Merge_Strings_Alternately.MergeAlternately("abcd", "pq"); 
+        string thirdCase = Merge_Strings_Alternately.MergeAlternately("abc", "xyz"); 
+
     }
 }
 
